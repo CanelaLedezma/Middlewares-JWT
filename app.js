92f4c395da47a1a313b6f9231dfe33e1401012c9
Android Studio <devandroid@ort>
2026-06-29 08:56:38 -0300

    agrego estructura base y configuración de entorno

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// Middlewares base
 app.use(cors());
 app.use(express.json());
 
-// Ruta de prueba
 app.get("/", (req, res) => {
   res.send("API de Instagram funcionando");
 });
 
-// Levanto el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
